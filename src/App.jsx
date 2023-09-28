8f71dd290880e0f90a4855e8c462457616bb140a
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-28 09:20:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="f9dfdd64-c739-4e96-8b89-85ba67249301"
+      userName="hardy"
+      userSecret="210504Hardy"
+    />
+  );
+};
+
+export default App;
